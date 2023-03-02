eb3017d7bc93bf5f62c00561df4c5127a358fa52
Your Name <you@example.com>
2023-03-02 15:43:10 +0600

    commit

---
+++ cmd/web/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(http.Dir("ui"))))
+	mux.HandleFunc("/", Home)
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+type Artists []struct {
+	ID            int                 `json:"id"`
+	Image         string              `json:"image"`
+	Name          string              `json:"name"`
+	Members       []string            `json:"members"`
+	CreationDate  int                 `json:"creationDate"`
+	FirstAlbum    string              `json:"firstAlbum"`
+	Locations     string              `json:"locations"`
+	ConcertDates  string              `json:"concertDates"`
+	DatesLocation map[string][]string `json:"datesLocations"`
+	Relations     string              `json:"relations"`
+}
+
+type Relation struct {
+	Index []struct {
+		ID            int                 `json:"id"`
+		DatesLocation map[string][]string `json:"datesLocations"`
+	} `json:"index"`
+}
+
+var (
+	Artist    []Artists
+	relations Relation
+)
+
+func Unmarshall() error {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	jsonErr := json.Unmarshal(body, &Artist)
+	if jsonErr != nil {
+		log.Println("jsonErr")
+	}
+	return nil
+}
